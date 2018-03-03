882ba1b19209a55f265a91e5ba702b8f0a549931
martincrrb <mcristo@shargo.io>
2018-03-03 13:48:47 +0100

    ADD navigate to new route and change theme to white

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Startup Name Generator',
       home: new RandomWords(),
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
     );
   }
 }
